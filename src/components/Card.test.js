36421f591c92d3b0444e8c21c31307802778206d
Kate Pond <kate@thepondsedge.com>
2018-06-13 11:11:32 -0700

    creates shallow snapshot tests for Card and Board components

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import Board from './Board';
+import Card from './Card';
 
-describe('Board', () => {
+describe('Card', () => {
   test('that it matches an existing snapshot', () => {
     // Mount the component in the DOM
     // Given
-    const wrapper = shallow(<Board />);
+    const wrapper = shallow(<Card />);
     // When and Then
     expect(wrapper).toMatchSnapshot();
     // Remove component from the DOM
